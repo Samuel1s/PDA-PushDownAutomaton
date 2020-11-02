a1fc2a11078c4d98b9f99c78d118c03fc38ca046
Samuel Santos <Samuel@MacBook-Pro-de-Samuel-Santos.local>
2020-11-02 17:23:38 -0300

    Término do TP.

--- src/apd.py
+++ src/apd.py
@@ -11,59 +11,60 @@
 import json
 import sys
 import os
- 
-class AUTOMATO:
-    def __init__(self):
-        self.stack = []
 
-    def compute(self, parsedData):
-        validar = True
-        palavra = input('Digite a palavra: ')
-        palavra = palavra.rstrip()
+class Validar:
+    def validarPilha(self, pilha, palavra, transicoes, estadoAtual, estadosFinais, simboloAtualPilha ):
+        for letra in palavra:
+            for transicao in transicoes:
+                if ((transicao[0] == estadoAtual) and (transicao[1] == letra) and (transicao[2] == simboloAtualPilha)):
+                    estadoAtual = transicao[3]
+                    if(len(transicao[4]) == 2):
+                        pilha.append(letra)
+                    elif(len(transicao[4]) == 3):
+                        pilha.append(letra)
+                        pilha.append(letra) 
+                    elif ((transicao[4] == '#') and (len(pilha) != 1)):
+                        pilha.pop()
+                        break   
+                    
+            simboloAtualPilha = pilha[len(pilha)-1]
+
+        if(estadoAtual in estadosFinais):
+            print('SIM')
+            pilha = []
+        else:
+            print('NÃO')
+            pilha = []  
+ 
+class Automato:
+    def definir(self, json):
+        pilha = [] 
+        validarPalavra = True
+        palavra = input()
 
         for letra in palavra:
-            if letra not in parsedData['ap'][1]:
-                validar = False
+            if letra not in json['ap'][1]:
+                validarPalavra = False
         
-        if (validar == True):
+        if (validarPalavra == True):
             palavra += '#'
-            simboloInicialPilha = parsedData['ap'][2][0]
-            self.stack.append(simboloInicialPilha)
-            estadosFinais = parsedData['ap'][5]
-            estadoInicial = parsedData['ap'][4]
-            simbolosPilha = parsedData['ap'][2]
-            transicoes = parsedData['ap'][3]
+            simboloInicialPilha = json['ap'][2][0]
+            pilha.append(simboloInicialPilha)
+            estadosFinais = json['ap'][5]
+            estadoInicial = json['ap'][4]
+            simbolosPilha = json['ap'][2]
+            transicoes = json['ap'][3]
 
             simboloAtualPilha = simboloInicialPilha
             estadoAtual = estadoInicial
-            
-            print('Estados\tEntrada\tPilha\tTransições')
-            print('{}\t {}\t {}\t ({}, {})'.format(estadoAtual, '_', 'Z', simboloAtualPilha, self.stack))
-            for letra in palavra:
-                for transicao in transicoes:
-                    if ((transicao[0] == estadoAtual) and (transicao[1] == letra) and (transicao[2] == simboloAtualPilha)):
-                        estadoAtual = transicao[3]
-                        if(len(transicao[4]) == 2):
-                            self.stack.append(letra)
-                        elif(len(transicao[4]) == 3):
-                            self.stack.append(letra)
-                            self.stack.append(letra) 
-                        elif ((transicao[4] == '#') and (len(self.stack) != 1)):
-                            self.stack.pop()
-                            break   
-                previousStackSymbol = simboloAtualPilha
-                simboloAtualPilha = self.stack[len(self.stack)-1]
-                print('{}\t {}\t {}\t ({}, {})'.format(estadoAtual, letra, previousStackSymbol, simboloAtualPilha, self.stack))
 
-            if(estadoAtual in estadosFinais):
-                print('SIM')
-                self.stack = []
-            else:
-                print('NÃO')
-                self.stack = []
+            validar_apd = Validar()
+            validar_apd.validarPilha(pilha, palavra, transicoes, estadoAtual, estadosFinais, simboloAtualPilha)
+
         else: 
             print('Palavra digitada não é válida.\n')
-def main():
+
+try:
     if os.path.exists(sys.argv[1]):
         f = open(sys.argv[1])
         data = json.load(f)
@@ -72,25 +73,10 @@ def main():
         print('Arquivo passado por parâmetro não encontrado.\n')
         sys.exit(0)    
 
-    apd = AUTOMATO()
+    apd = Automato()
 
-    print('Carregando dados do json passado por parâmetro: ')
-    print('--------------------------------------')
-    print('Conjunto de estados: ', data['ap'][0])
-    print('Alfabeto de símbolos: ', data['ap'][1])
-    print('Alfabeto da pilha: ', data['ap'][2])
-    print('Lista de Transições:')
-    for transicao in data['ap'][3]:
-        print('\t', transicao)
-    print('Estado inicial: ', data['ap'][4])
-    print('Conjunto de estados finais: ', data['ap'][5])
-    print('--------------------------------------')
-     
     while 1:  
-        apd.compute(data)
-
-if __name__ == '__main__':
-    try:
-        main()
-    except (KeyboardInterrupt) as e:
-        sys.exit(0)    
+        apd.definir(data)
+        
+except (KeyboardInterrupt) as e:
+    sys.exit(0)    
